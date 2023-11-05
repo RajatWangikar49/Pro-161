e641833f46d498b82f4ecfe383026264f88b275b
RajatWangikar49 <84658365+RajatWangikar49@users.noreply.github.com>
2023-11-05 12:18:24 +0530

    Add files via upload

---
+++ Throw.js
@@ -0,0 +1,26 @@
+AFRAME.registerComponent("bowling-balls", {
+
+    init : function(){
+        this.throwBalls();
+    },
+
+    throwBalls : function(){
+        window.addEventListener("keydown", (e)=>{
+            if (e.key === "z"){
+                var ball = document.createElement("a-entity");
+                ball.setAttribute("geometry", {primitive : "sphere", radius : "0.3"});
+                ball.setAttribute("material", {color : "white"});
+                var cam = document.querySelector("#camera");
+                var pos = cam.getAttribute("position");
+                ball.setAttribute("position", {x : pos.x, y : pos.y, z : pos.z});
+                var camera = document.querySelector("#camera").object3D;
+                var direction = new THREE.Vector3();
+                camera.getWorldDirection(direction);
+                ball.setAttribute("velocity", direction.multiplyScalar(-10));
+                var scene = document.querySelector("#scene");
+                scene.appendChild(ball);
+            }
+        })
+    }
+
+})
